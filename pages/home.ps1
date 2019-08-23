f8ecf54c9926124f11fa40865d36c7cb079814f2
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-23 14:36:17 -0400

    updated content

--- pages/home.ps1
+++ pages/home.ps1
@@ -7,7 +7,12 @@
             New-UDHtml -Markup "<div class='left-align'><h2>$(Get-Date -f 'dddd MMMM dd, yyyy')</h2>
 <p>Configuration Manager Web Toolkit is built on Universal Dashboard Community Edition.</p>
 <p>Visit <a href=`"`" target=`"_blank`">xx</a> for more information.</p>
-<p>CMWT is open source at <a href=`"https://github.com/Skatterbrainz/ud-cmwt`" target=`"_blank`">GitHub</a></p>"
+<p>CMWT is open source at <a href=`"https://github.com/Skatterbrainz/ud-cmwt`" target=`"_blank`">GitHub</a></p></div>
+<div class='left-align'>
+<p>If you would like to contribute to this project, please submit code changes through pull requests.</p>
+<p>If you don't want to write any code, please submit ideas/bugs using the 'Issues' on the GitHub repo.</p>
+<p>Thank you!</p>
+</div>"
         }
     }
 }
