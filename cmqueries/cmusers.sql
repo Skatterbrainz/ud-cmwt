f8b3f2a475ecded954c299fb3f39da7df4140cd7
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-24 17:57:16 -0400

    testing new query isolation model

---
+++ cmqueries/cmusers.sql
@@ -0,0 +1,10 @@
+select distinct
+    ResourceID,
+    User_Name0 AS UserName,
+    User_Principal_Name0 AS UPN,
+    SID0 AS SID, Full_User_Name0 AS FullName,
+    AD_Object_Creation_Time0 AS DateCreated
+from
+    dbo.v_R_User AS us
+order by
+    Full_User_Name0
