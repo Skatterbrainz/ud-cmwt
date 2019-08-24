5028a456815251bc503830835c0b1ff4f56172ed
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-23 18:12:16 -0700

    fixed property values display

--- pages/services.ps1
+++ pages/services.ps1
@@ -1,6 +1,12 @@
-﻿New-UDPage -Name "Services" -Icon tachometer -Content { 
+﻿New-UDPage -Name "Services" -Icon tachometer -Content {
     New-UDGrid -Title "Services" -Endpoint {
-        Get-Service -ComputerName $Cache:ConnectionInfo.Server | 
-            Select ServiceName,DisplayName,Status,StartType | Out-UDGridData
+        Get-Service -ComputerName $Cache:ConnectionInfo.Server | ForEach-Object {
+            [pscustomobject]@{
+                DisplayName = [string]$_.DisplayName
+                ServiceName = [string]$_.ServiceName
+                StartType   = [string]$_.StartType
+                Status      = [string]$_.Status
+            }
+        } | Out-UDGridData
     }
 }
