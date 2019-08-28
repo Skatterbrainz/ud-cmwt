5359b97c82d98fffdaef3539d9120c5dcd863243
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-28 10:40:48 -0700

    minor formatting updates

--- pages/cmdepsummary.ps1
+++ pages/cmdepsummary.ps1
@@ -1,11 +1,10 @@
-New-UDPage -Name "CMDepSummary" -Id 'cmdepsummary' -Content {
+New-UDPage -Name "cmdepsummary" -Id 'cmdepsummary' -Content {
 	New-UDGrid -Title "Configuration Manager Deployment Summaries" -Endpoint {
         $qname    = "cmdepsummary.sql"
         $SiteHost = $Cache:ConnectionInfo.Server
         $Database = $Cache:ConnectionInfo.CmDatabase
 		$BasePath = $Cache:ConnectionInfo.QfilePath
 		$qfile    = Join-Path $BasePath $qname
-        Invoke-DbaQuery -SqlInstance $SiteHost -Database $Database -File $qfile |
-            Out-UDGridData
+        Invoke-DbaQuery -SqlInstance $SiteHost -Database $Database -File $qfile | Out-UDGridData
     }
 }
