fe78f94e621e983d16a97c022c4efae22ac1b577
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-31 12:24:30 -0700

    updates to search code - needs more work

--- pages/cmfind.ps1
+++ pages/cmfind.ps1
@@ -3,28 +3,34 @@ New-UDPage -Url "/cmfind/:cmclass/:prop/:value" -Endpoint {
     switch ($prop) {
         'osname'  {
             $pagetitle = "Devices by Operating System"
-            $qname = "cmdevices.sql"
+            $qname     = "cmdevices.sql"
+            $proplist  = @("Name","ResourceID","Model","OSName","OSBuild","Client","ADSite")
         }
         'osbuild' {
             $pagetitle = "Devices by Operating System BuildNumber"
-            $qname = "cmdevices.sql"
+            $qname     = "cmdevices.sql"
+            $proplist  = @("Name","ResourceID","Model","OSName","OSBuild","Client","ADSite")
         }
         'model'   {
             $pagetitle = "Devices by Model"
-            $qname = "cmdevices.sql"
+            $qname     = "cmdevices.sql"
+            $proplist  = @("Name","ResourceID","Model","OSName","OSBuild","Client","ADSite")
         }
         'mfr'     {
             $pagetitle = "Devices by Manufacturer"
-            $qname = "cmdevice.sql"
+            $qname     = "cmdevice.sql"
+            $proplist  = @("Name","ResourceID","Model","OSName","OSBuild","Client","ADSite")
         }
         'adsitename' {
             if ($cmclass -eq 'device') {
                 $pagetitle = "Devices by AD Site"
-                $qname = "cmdevices.sql"
+                $qname     = "cmdevices.sql"
+                $proplist  = @("Name","ResourceID","Model","OSName","OSBuild","Client","ADSite")
             }
             else {
                 $pagetitle = "Users by AD Site"
-                $qname = "cmusers.sql"
+                $qname     = "cmusers.sql"
+                $proplist  = @("UserName","ResourceID","DisplayName")
             }
         }
         default   { $pagetitle = "Error" }
@@ -35,22 +41,50 @@ New-UDPage -Url "/cmfind/:cmclass/:prop/:value" -Endpoint {
     $Database = $Cache:ConnectionInfo.CmDatabase
     $BasePath = $Cache:ConnectionInfo.QfilePath
     $qfile    = Join-Path $BasePath $qname
+    $cdata = @(Invoke-DbaQuery -SqlInstance $SiteHost -Database $Database -File $qfile | Where-Object {$_."$prop" -eq $sval})
+
     New-UDRow {
         New-UDGrid -Title "$pagetitle ($sval)" -Endpoint {
-            $cdata = @(Invoke-DbaQuery -SqlInstance $SiteHost -Database $Database -File $qfile | Where-Object {$_."$prop" -eq $sval})
-            $cdata | Foreach-Object {
-                    $resid = [string]$_.ResourceID
-                    $name  = [string]$_.Name
-                    [pscustomobject]@{
-                        Name    = New-UDElement -Tag "a" -Attributes @{ href="/cmdevice/$resid/1" } -Content { $name }
-                        ResourceID = $resid
-                        OSName  = [string]$_.OSName
-                        OSBuild = [string]$_.OSBuild
-                        Client  = [string]$_.ClientVersion
-                        ADSite  = [string]$_.ADSiteName
-                        Model   = [string]$_.Model
-                    }
-                } | Out-UDGridData
-        }
-    }
+            switch ($prop) {
+                'model' {
+                    $cdata | Foreach-Object {
+                        $resid   = [string]$_.ResourceID
+                        $name    = [string]$_.Name
+                        $model   = [string]$_.Model
+                        $osname  = [string]$_.OSName
+                        $osbuild = [string]$_.OSBuild
+                        $adsite  = [string]$_.ADSiteName
+                        [pscustomobject]@{
+                            Name    = New-UDElement -Tag "a" -Attributes @{ href="/cmdevice/$resid/1" } -Content { $name }
+                            ResourceID = $resid
+                            OSName  = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/osname/$osname" } -Content { $osname }
+                            OSBuild = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/osbuild/$osbuild" } -Content { $osbuild }
+                            Client  = [string]$_.ClientVersion
+                            ADSite  = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/adsite/$adsite" } -Content { $adsite }
+                            Model   = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/model/$model" } -Content { $model }
+                        }
+                    } | Out-UDGridData
+                }
+                'osname' {
+                    $cdata | Foreach-Object {
+                        $resid   = [string]$_.ResourceID
+                        $name    = [string]$_.Name
+                        $model   = [string]$_.Model
+                        $osname  = [string]$_.OSName
+                        $osbuild = [string]$_.OSBuild
+                        $adsite  = [string]$_.ADSiteName
+                        [pscustomobject]@{
+                            Name    = New-UDElement -Tag "a" -Attributes @{ href="/cmdevice/$resid/1" } -Content { $name }
+                            ResourceID = $resid
+                            OSName  = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/osname/$osname" } -Content { $osname }
+                            OSBuild = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/osbuild/$osbuild" } -Content { $osbuild }
+                            Client  = [string]$_.ClientVersion
+                            ADSite  = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/adsite/$adsite" } -Content { $adsite }
+                            Model   = New-UDElement -Tag "a" -Attributes @{ href="/cmfind/device/model/$model" } -Content { $model }
+                        }
+                    } | Out-UDGridData
+                }
+            } # switch
+        } # grid
+    } # row
 }
