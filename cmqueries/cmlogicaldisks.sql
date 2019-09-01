fbdf235ba31f7a5ba2e601a6ce263bda74160c19
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-09-01 13:01:20 -0700

    fixed boneheaded mispelling

--- cmqueries/cmlogicaldisks.sql
+++ cmqueries/cmlogicaldisks.sql
@@ -7,7 +7,7 @@ SELECT DISTINCT
 	COALESCE (FreeSpace0, 0) as FreeSpace,
 	(COALESCE (Size0, 0) - COALESCE (FreeSpace0, 0)) as Used,
 	FileSystem0 as FileSystem,
-	case when (Compressed0 = 1) then 'Yes' else 'No' end as Copmressed,
+	case when (Compressed0 = 1) then 'Yes' else 'No' end as Compressed,
     VolumeSerialNumber0 as SerialNum
 FROM v_GS_LOGICAL_DISK
 ORDER BY ResourceID
